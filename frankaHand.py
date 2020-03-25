55531d51a12b8193072fcf3a5d11866f19c162d2
Michael Lin <michaelv03@gmail.com>
2020-03-24 18:09:20 -0700

    adding base primatic joints. Also made gripper to be contrained to one DOF

--- frankaHand.py
+++ frankaHand.py
@@ -8,13 +8,17 @@ import time
 
 class FrankaHand:
 
-  def __init__(self, urdfRootPath=pybullet_data.getDataPath(), timeStep=0.01):
+  def __init__(self, urdfRootPath=pybullet_data.getDataPath(), 
+                initPos=[0.00000,0.00000,0.50000],timeStep=0.01):
     self.urdfRootPath = urdfRootPath
     self.timeStep = timeStep
+    self.initPos = initPos
     self.maxVelocity = .35
-    self.maxForce = 200.
+    self.maxForce = 20.
     self.fingerForce = 2
-    self.fingerIndices = [0, 1]
+    self.fingerIndices = [2, 3]
+    self.leftFingerIndex = 2
+    self.rightFingerIndex = 3
     self.useSimulation = 1
     self.reset()
 
@@ -23,26 +27,31 @@ class FrankaHand:
     self.gripperUid = objects[0]
   
     # set the position of the base to be on the table
-    p.resetBasePositionAndOrientation(self.gripperUid, [-0.100000, 0.000000, -0.150000],
+    p.resetBasePositionAndOrientation(self.gripperUid, self.initPos,
                                       [0.000000, 0.000000, 0.000000, 1.000000])
 
     # set mass of base body to 0 so it is fixed to the world (-1 is index for # base)
     p.changeDynamics(self.gripperUid, -1, mass = 0.) 
 
+    p.resetJointState(self.gripperUid, self.leftFingerIndex, 0)
+    p.setJointMotorControl2(self.gripperUid,
+                            self.leftFingerIndex,
+                            p.POSITION_CONTROL,
+                            targetPosition=0.04,
+                            force=self.maxForce)
 
-    for jointIndex in self.fingerIndices:
-        p.resetJointState(self.gripperUid, jointIndex, 0)
-        p.setJointMotorControl2(self.gripperUid,
-                                jointIndex,
-                                p.POSITION_CONTROL,
-                                targetPosition=0.2,
-                                force=self.maxForce)
+    p.resetJointState(self.gripperUid, self.rightFingerIndex, 0)
+    p.setJointMotorControl2(self.gripperUid,
+                            self.rightFingerIndex,
+                            p.POSITION_CONTROL,
+                            targetPosition=0.04,
+                            force=self.maxForce)
 
     #self.trayUid = p.loadURDF(os.path.join(self.urdfRootPath, "tray/tray.urdf"), 0.640000,
     #                          0.075000, -0.190000, 0.000000, 0.000000, 1.000000, 0.000000)
 
-    self.motorName = "N/A"
-    self.motorIndex = 0
+    #self.motorName = "N/A"
+    #self.motorIndex = 0
 
     #for i in range(self.numJoints):
     #  jointInfo = p.getJointInfo(self.gripperUid, i)
@@ -53,12 +62,20 @@ class FrankaHand:
     #    self.motorIndex = i
 
   def applyAction(self, motorCommand):
-      for i, jointIndex in enumerate(self.fingerIndices):
-          p.setJointMotorControl2(self.gripperUid,
-                                  jointIndex,
-                                  p.POSITION_CONTROL,
-                                  targetPosition=motorCommand[i],
-                                  force=self.maxForce)
+      # only actuated one finger and mimic with the other one
+      p.setJointMotorControl2(self.gripperUid,
+                              self.leftFingerIndex,
+                              p.POSITION_CONTROL,
+                              targetPosition=motorCommand,
+                              force=self.maxForce)
+
+      # get position of left finger (position, velocity, joint forces, motor torque)
+      (pos, vel, f, t) = p.getJointState(self.gripperUid, self.leftFingerIndex)
+      p.setJointMotorControl2(self.gripperUid,
+                              self.rightFingerIndex,
+                              p.POSITION_CONTROL,
+                              targetPosition=pos,
+                              force=self.maxForce)
 
 
 if __name__ == "__main__":
@@ -67,7 +84,7 @@ if __name__ == "__main__":
   stepCount = 0
   while (stepCount < 100000):
       fingerPos = 0.02*(np.sin(2*np.pi*stepCount/100)+1)
-      h.applyAction([fingerPos]*2)
+      #h.applyAction([fingerPos]*2)
       stepCount += 1
       p.stepSimulation()
       time.sleep(h.timeStep)
--- frankaHandGymEnv.py
+++ frankaHandGymEnv.py
@@ -39,17 +39,28 @@ class FrankaHandGymEnv(gym.Env):
     p.loadURDF(os.path.join(self._urdfRoot, "table/table.urdf"), 0.5000000, 0.00000, -.820000,
                0.000000, 0.000000, 0.0, 1.0)
 
-    xpos = 0.55 + 0.12 * random.random()
-    ypos = 0 + 0.2 * random.random()
-    ang = 3.14 * 0.5 + 3.1415925438 * random.random()
-    orn = p.getQuaternionFromEuler([0, 0, ang])
-    self.blockUid = p.loadURDF(os.path.join(self._urdfRoot, "block.urdf"), xpos, ypos, -0.15,
-                               orn[0], orn[1], orn[2], orn[3])
-
     p.setGravity(0, 0, -10)
-    self._gripper = FrankaHand(urdfRootPath = self._urdfRoot, timeStep = self._timeStep)
+    self._gripper = FrankaHand(urdfRootPath = self._urdfRoot, 
+                      initPos = [0.00000, 0.0000, -0.2], timeStep = self._timeStep)
+    # step once first to place the gripper in place
+    for i in range(100):
+        p.stepSimulation()
+        time.sleep(self._timeStep)
 
+    xpos = 0.15 - np.abs(0.03 * random.random())
+    ypos = 0 + 0.04 * random.random()
+    ang = 3.14 * 0.5 + 3.1415925438 * random.random()
+    orn = p.getQuaternionFromEuler([0, 1.57079632679, ang])
+    self.blockUid = p.loadURDF(os.path.join(self._urdfRoot, "block.urdf"), 
+                                xpos, ypos, -0.15,
+                                orn[0], orn[1], orn[2], orn[3])
+
+
+    p.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw = -90., cameraPitch = -89.,
+    #p.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw = 45., cameraPitch = 0.,
+                      cameraTargetPosition=[0.1, 0., 0.])
     p.stepSimulation()
+
 
   def __del__(self):
     p.disconnect()
@@ -64,9 +75,14 @@ class FrankaHandGymEnv(gym.Env):
 if __name__ == "__main__":
   fh_env = FrankaHandGymEnv(maxSteps=10000)
   stepCount = 0
+  closed = False
   while (stepCount < fh_env._maxSteps):
       fingerPos = 0.02*(np.sin(2*np.pi*stepCount/100)+1)
-      fh_env._gripper.applyAction([fingerPos]*2)
+      if (stepCount > 1000):
+          if (not closed):
+              fh_env._gripper.applyAction(fingerPos)
+          if (fingerPos == 0):
+              closed = True
       stepCount += 1
       p.stepSimulation()
       time.sleep(fh_env._timeStep)
