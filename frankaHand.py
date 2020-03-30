200f8b9c78477d7fab8ba13214d575ec977dde4e
Michael Lin <michaelv03@gmail.com>
2020-03-30 16:48:32 -0700

    got the experiment setup to pick up blocks

--- frankaHand.py
+++ frankaHand.py
@@ -16,35 +16,50 @@ class FrankaHand:
     self.maxVelocity = .35
     self.maxForce = 20.
     self.fingerForce = 2
-    self.leftFingerIndex = 0
-    self.rightFingerIndex = 1
-    self.useSimulation = 1
+    self.leftFingerIndex = 2
+    self.rightFingerIndex = 4
+    self.leftFingerPadIndex = 3
+    self.rightFingerPadIndex = 5
+    self.baseZIndex = 0
+    self.baseXIndex = 1
     self.reset()
 
   def reset(self):
-    objects = p.loadSDF(os.path.join(self.urdfRootPath, "franka_panda/panda_hand.sdf"))
-    self.gripperUid = objects[0]
-  
+    temp = p.loadSDF(os.path.join(self.urdfRootPath, "franka_panda/panda_hand2.sdf"),
+                        useMaximalCoordinates = False)
+    self.gripperUid = temp[0]
+
     # set the position of the base to be on the table
     p.resetBasePositionAndOrientation(self.gripperUid, self.initPos,
                                       [0.000000, 0.000000, 0.000000, 1.000000])
 
     # set mass of base body to 0 so it is fixed to the world (-1 is index for # base)
-    p.changeDynamics(self.gripperUid, -1, mass = 0.) 
+    #p.changeDynamics(self.gripperUid, -1, mass = 0.) 
 
     # reset fingers to original position
     p.setJointMotorControl2(self.gripperUid,
                             self.leftFingerIndex,
                             p.POSITION_CONTROL,
-                            targetPosition=0.,
+                            targetPosition=0.04,
                             force=self.maxForce)
 
     # for gear constraint right finger must be in position control with no force limit
     p.setJointMotorControl2(self.gripperUid,
                             self.rightFingerIndex,
                             p.POSITION_CONTROL,
-                            targetPosition=0.,
+                            targetPosition=0.04,
                             force=0)
+
+    p.setJointMotorControl2(self.gripperUid, self.baseZIndex,
+                            p.POSITION_CONTROL,
+                            targetPosition=0.0,
+                            force=0.0)
+
+    # move the prismatic wrist to 0
+    p.setJointMotorControl2(self.gripperUid, self.baseXIndex,
+                            controlMode=p.POSITION_CONTROL, 
+                            targetPosition = 0,
+                            force=100.0)
 
     # create the gearing contraint. Make right finger
     # child of left finger.
@@ -58,6 +73,21 @@ class FrankaHand:
                            childFramePosition=[0, 0, 0])
     p.changeConstraint(c, gearRatio=-1, maxForce=10000)
 
+    self.enableFingerSensor()
+
+
+
+  def enableFingerSensor(self):
+      p.enableJointForceTorqueSensor(self.gripperUid, self.leftFingerPadIndex)
+      p.enableJointForceTorqueSensor(self.gripperUid, self.rightFingerPadIndex)
+
+  def getFingerForce(self):
+      leftState = p.getJointState(self.gripperUid, self.leftFingerPadIndex)
+      rightState = p.getJointState(self.gripperUid, self.rightFingerPadIndex)
+      #print("left {}".format(leftState[2][:3]))
+      #print("right {}".format(rightState[2][:3]))
+      #print("left {}".format(leftState[2][:3]))
+      print("right {}".format(rightState[2]))
 
   def applyAction(self, motorCommand):
       # only actuated one finger and mimic with the other one
@@ -67,6 +97,51 @@ class FrankaHand:
                               targetPosition=motorCommand,
                               force=self.maxForce)
 
+  def liftObject(self):
+      # only actuated one finger and mimic with the other one
+      p.setJointMotorControl2(self.gripperUid,
+                              self.baseZIndex,
+                              p.VELOCITY_CONTROL,
+                              targetVelocity=0.2,
+                              velocityGain=0.5,
+                              force=500.0)
+  def stopLiftObject(self):
+      p.setJointMotorControl2(self.gripperUid,
+                              self.baseZIndex,
+                              p.VELOCITY_CONTROL,
+                              targetVelocity=0.0,
+                              velocityGain=0.5,
+                              force=500.0)
+
+      zState = p.getJointState(self.gripperUid, self.baseZIndex)
+      p.setJointMotorControl2(self.gripperUid,
+                              self.baseZIndex,
+                              p.POSITION_CONTROL,
+                              targetPosition=zState[0],
+                              force=500.0)
+
+  def resetPose(self):
+      # move z prismatic back to 0
+      p.setJointMotorControl2(self.gripperUid,
+                              self.baseZIndex,
+                              p.POSITION_CONTROL,
+                              targetPosition=0.0,
+                              force=500.0)
+
+      # reset fingers to original position
+      p.setJointMotorControl2(self.gripperUid,
+                              self.leftFingerIndex,
+                              p.POSITION_CONTROL,
+                              targetPosition=0.04,
+                              force=self.maxForce)
+
+      # for gear constraint right finger must be in position control with no force limit
+      p.setJointMotorControl2(self.gripperUid,
+                              self.rightFingerIndex,
+                              p.POSITION_CONTROL,
+                              targetPosition=0.04,
+                              force=0)
+
 if __name__ == "__main__":
   p.connect(p.GUI)
   urdfRoot=pybullet_data.getDataPath()
@@ -74,8 +149,10 @@ if __name__ == "__main__":
   h = FrankaHand()
   stepCount = 0
   p.setRealTimeSimulation(1)
+  time.sleep(1)
   while (1):
       fingerPos = 0.02*(np.sin(2*np.pi*stepCount/100)+1)
       h.applyAction(fingerPos)
       stepCount += 1
       time.sleep(h.timeStep)
+
--- frankaHandGymEnv.py
+++ frankaHandGymEnv.py
@@ -21,9 +21,12 @@ class FrankaHandGymEnv(gym.Env):
     self._isEnableSelfCollision = isEnableSelfCollision
     self._envStepCounter = 0
     self._maxSteps = maxSteps
-    self._cam_dist = 1.3
-    self._cam_yaw = 180
-    self._cam_pitch = -40
+
+    # camera parameters
+    self._camDist = 0.25
+    self._camYaw = -90
+    self._camPitch = -89
+    self._camTargetPosition = [0.1, 0., 0.]
 
     self._p = p
     p.connect(p.GUI)
@@ -34,36 +37,72 @@ class FrankaHandGymEnv(gym.Env):
     p.resetSimulation()
     p.setPhysicsEngineParameter(numSolverIterations=150)
     p.setTimeStep(self._timeStep)
-    p.loadURDF(os.path.join(self._urdfRoot, "plane.urdf"), [0, 0, -1])
 
-    p.loadURDF(os.path.join(self._urdfRoot, "table/table.urdf"), 0.5000000, 0.00000, -.820000,
-               0.000000, 0.000000, 0.0, 1.0)
+    # load plane 1 meter below 0
+    p.loadURDF(os.path.join(self._urdfRoot, "plane.urdf"), [0, 0, -2])
+
+    # load experiment table
+    self.tableUid = p.loadURDF(os.path.join(self._urdfRoot, "table/table.urdf"), 
+              0.5000000, 0.00000, -.820000,
+               0.0000, 0.0000, 0.0, 1.0)
+
+    # change the view point
+    p.resetDebugVisualizerCamera(cameraDistance=self._camDist, cameraYaw=self._camYaw, 
+                      cameraPitch=self._camPitch, cameraTargetPosition=self._camTargetPosition)
 
     p.setGravity(0, 0, -10)
+
+    # instantiate the franka Gripper
     self._gripper = FrankaHand(urdfRootPath = self._urdfRoot, 
                       initPos = [0.00000, 0.0000, -0.2], timeStep = self._timeStep)
-    # step once first to place the gripper in place
-    for i in range(100):
-        p.stepSimulation()
-        time.sleep(self._timeStep)
 
-    xpos = 0.15 - np.abs(0.03 * random.random())
-    ypos = 0 + 0.04 * random.random()
+    # step once first to place the gripper in place
+    self.sleepSim(1)
+
+    self.placeRandBlock()
+
+    self.sleepSim(1)
+
+  def placeRandBlock(self):
+    # randomly generate a 3d pose of the block to grasp
+    # limit is at xpos = 0.215, center is xpos = 0.195
+    xpos = 0.195 + 0.035 * (random.random()-0.5)
+    ypos = 0 + 0.056 * (random.random()-0.5)
     ang = 3.14 * 0.5 + 3.1415925438 * random.random()
     orn = p.getQuaternionFromEuler([0, 1.57079632679, ang])
     self.blockUid = p.loadURDF(os.path.join(self._urdfRoot, "block.urdf"), 
-                                xpos, ypos, -0.15,
+                                xpos, ypos, -0.0149,
                                 orn[0], orn[1], orn[2], orn[3])
 
+  def getBlockHeight(self):
+      pose = p.getBasePositionAndOrientation(self.blockUid)
+      return pose[0][2]
+  
+  def resetExp(self):
+    # remove the block
+    p.removeBody(self.blockUid)
 
-    p.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw = -90., cameraPitch = -89.,
-    #p.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw = 45., cameraPitch = 0.,
-                      cameraTargetPosition=[0.1, 0., 0.])
-    p.stepSimulation()
+    # reset gripper position
+    self._gripper.resetPose()
+
+    # step once first to place the gripper in place
+    self.sleepSim(1)
+
+    self.placeRandBlock()
+
+    self.sleepSim(1)
 
 
   def __del__(self):
     p.disconnect()
+
+  def sleepSim(self, sleepTime):
+    """ Step through the simluation for some amount of time. """
+    steps = int(round(sleepTime / self._timeStep))
+    # step once first to place the gripper in place
+    for i in range(steps):
+        p.stepSimulation()
+        time.sleep(self._timeStep)
 
   def step(self, action):
     dv = 0.005
@@ -73,19 +112,46 @@ class FrankaHandGymEnv(gym.Env):
 
 
 if __name__ == "__main__":
-  fh_env = FrankaHandGymEnv(maxSteps=10000)
   stepCount = 0
   closed = False
-  p.setRealTimeSimulation(1)
-  while (stepCount < fh_env._maxSteps):
-      fingerPos = 0.02*(np.sin(2*np.pi*stepCount/100)+1)
+  fh_env = FrankaHandGymEnv(maxSteps=10000)
+  total_attempts = 0
+  total_success = 0
+  while (True):
+      total_attempts += 1
+      #fingerPos = 0.02*(np.sin(2*np.pi*stepCount/1000)+1)
 
-      #fh_env._gripper.applyAction(fingerPos)
-      #if (stepCount > 1000):
-      #    if (not closed):
-      #        fh_env._gripper.applyAction(fingerPos)
-      #    if (fingerPos == 0):
-      #        closed = True
-      stepCount += 1
-      #p.stepSimulation()
-      time.sleep(fh_env._timeStep)
+      #if (fingerPos > 0.02):
+      #    p.setJointMotorControl2(fh_env._gripper.gripperUid,
+      #                            1,
+      #                            p.TORQUE_CONTROL,
+      #                            force=2.)
+      #else:
+      #    p.setJointMotorControl2(fh_env._gripper.gripperUid,
+      #                            1,
+      #                            p.TORQUE_CONTROL,
+      #                            force=-2.)
+
+      #if (stepCount % 2) == 0:
+      #    fh_env._gripper.getFingerForce()
+      #p.setJointMotorControl2(fh_env._gripper.gripperUid,
+      #                        1,
+      #                        p.PD_CONTROL,
+      #                        targetPosition = fingerPos*20,
+      #                        targetVelocity = 1.0,
+      #                        force=1.,
+      #                        positionGain=20,
+      #                        velocityGain=0.1)
+
+
+      fh_env._gripper.applyAction(0)
+      fh_env.sleepSim(1) 
+      fh_env._gripper.liftObject()
+      fh_env.sleepSim(0.5) 
+      fh_env._gripper.stopLiftObject()
+      fh_env.sleepSim(0.2) 
+      if (fh_env.getBlockHeight() > -0.05):
+          total_success += 1
+      print("lifted {}% trials".format(100.0*total_success/total_attempts))
+      fh_env.sleepSim(1) 
+      fh_env.resetExp()
